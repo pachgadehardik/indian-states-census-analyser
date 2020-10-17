b55fac2908697df9c97a15bf6141e73ddc5cf418
pachgadehardik <pachgadehardik@gmail.com>
2020-10-17 23:08:05 +0530

    Added 3rd TestCase to check for Incorrect Types

--- src/test/java/com/capg/censusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/capg/censusanalysertest/StateCensusAnalyserTest.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
+import com.capg.censusanalyser.CSVStateCensus;
 import com.capg.censusanalyser.CensusAnalyserException;
+import com.capg.censusanalyser.IncorrectPOJO;
 import com.capg.censusanalyser.StateCensusAnalyser;
 
 public class StateCensusAnalyserTest {
@@ -16,28 +18,34 @@ public class StateCensusAnalyserTest {
 	final String INCORRECT_DATA_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IncorrectFile.txt";
 
 	@Test
-	public void givenStateCensusFileHasCorrectNumberEntries_ShouldReturnTrue() throws CensusAnalyserException {
-		try {
-			assertEquals(29, StateCensusAnalyser.readCSVDataFile(INDIA_STATE_CENSUS_FILE));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void givenStateCensusFileHasCorrectNumberEntries_ShouldReturnTrue() throws CensusAnalyserException, IOException {
+			assertEquals(29, StateCensusAnalyser.readCSVDataFile(INDIA_STATE_CENSUS_FILE,CSVStateCensus.class));
 	}
 
 	@Test
 	public void givenIncorrectFilePathShouldReturnCustomException() throws CensusAnalyserException {
 
 		assertThrows(CensusAnalyserException.class, () -> {
-			StateCensusAnalyser.readCSVDataFile(INCORRECT_DATA_FILE);
+			StateCensusAnalyser.readCSVDataFile(INCORRECT_DATA_FILE,CSVStateCensus.class);
 		});
 
+//		try {
+//			StateCensusAnalyser.readCSVDataFile(INCORRECT_DATA_FILE,CSVStateCensus.class);
+//		} catch (CensusAnalyserException e) {
+//			e.printStackTrace();
+//			assertEquals(CensusAnalyserException.CensusExceptionType.FILE_NOT_FOUND_TYPE, e.type);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+	}
+	
+	@Test
+	public void givenCorrectCSVFile_IncorrectType_ShouldReturnCustomExeption() throws CensusAnalyserException, IOException {
 		try {
-			StateCensusAnalyser.readCSVDataFile(INCORRECT_DATA_FILE);
-		} catch (CensusAnalyserException e) {
-			e.printStackTrace();
-			assertEquals(CensusAnalyserException.CensusExceptionType.FILE_NOT_FOUND_TYPE, e.type);
-		} catch (IOException e) {
-			e.printStackTrace();
+			StateCensusAnalyser.readCSVDataFile(INDIA_STATE_CENSUS_FILE, IncorrectPOJO.class);
+		}
+		catch(CensusAnalyserException e) {
+			assertEquals(CensusAnalyserException.CensusExceptionType.INCORRECT_TYPE, e.type);
 		}
 	}
 
