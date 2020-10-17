1f5730d18133450d49e64f10a60f8d14b2514abb
pachgadehardik <pachgadehardik@gmail.com>
2020-10-17 20:57:59 +0530

    Added 2nd TestCase for Incorrect File Custom Exception

--- src/test/java/com/capg/censusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/capg/censusanalysertest/StateCensusAnalyserTest.java
@@ -1,21 +1,41 @@
 package com.capg.censusanalysertest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
+import com.capg.censusanalyser.CensusAnalyserException;
 import com.capg.censusanalyser.StateCensusAnalyser;
 
 public class StateCensusAnalyserTest {
 
-	private static final String INDIA_STATE_CENSUS_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IndiaStateCensusData - IndiaStateCensusData.csv";
+	final String INDIA_STATE_CENSUS_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IndiaStateCensusData - IndiaStateCensusData.csv";
+	final String INCORRECT_DATA_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IncorrectFile.txt";
 
 	@Test
-	public void givenStateCensusFileHasCorrectNumberEntries_ShouldReturnTrue() {
+	public void givenStateCensusFileHasCorrectNumberEntries_ShouldReturnTrue() throws CensusAnalyserException {
 		try {
 			assertEquals(29, StateCensusAnalyser.readCSVDataFile(INDIA_STATE_CENSUS_FILE));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenIncorrectFilePathShouldReturnCustomException() throws CensusAnalyserException {
+
+		assertThrows(CensusAnalyserException.class, () -> {
+			StateCensusAnalyser.readCSVDataFile(INCORRECT_DATA_FILE);
+		});
+
+		try {
+			StateCensusAnalyser.readCSVDataFile(INCORRECT_DATA_FILE);
+		} catch (CensusAnalyserException e) {
+			e.printStackTrace();
+			assertEquals(CensusAnalyserException.CensusExceptionType.FILE_NOT_FOUND_TYPE, e.type);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
