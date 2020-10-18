8ee6bb3f6062e7233c6c1abe9289764b07f28aa0
pachgadehardik <pachgadehardik@gmail.com>
2020-10-18 15:39:01 +0530

    Refactored the Analyser functionality

--- src/main/java/com/capg/censusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/capg/censusanalyser/StateCensusAnalyser.java
@@ -6,54 +6,55 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.stream.StreamSupport;
 
 import com.capg.censusanalyser.CensusAnalyserException.CensusExceptionType;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class StateCensusAnalyser {
-
+	
 	private final static String INDIA_STATE_CENSUS_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IndiaStateCensusData - IndiaStateCensusData.csv";
 	private final String INCORRECT_DATA_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IncorrectFile.txt";
 
-	public static <T> int readCSVDataFile(String dataFile, Class<T> myClass) throws IOException, CensusAnalyserException {
-		int countEntries = 0;
+	public static <T> int readCSVDataFile(String dataFile, Class<T> myClass)
+			throws IOException, CensusAnalyserException {
+		int count = 0;				
+		int countEntries =0;
+
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(dataFile));
 			validateInputObjectType(myClass);
-			
-			CsvToBean<T> csvToBean = new CsvToBeanBuilder<>(reader)
-					.withType((Class) myClass)
-					.withIgnoreLeadingWhiteSpace(true)
-					.build();
+
+			CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType((Class) myClass);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<T> csvToBean = csvToBeanBuilder.build();
 			Iterator<T> objectIterator = csvToBean.iterator();
-			 
-			while (objectIterator.hasNext()) {
-				Object myObject = objectIterator.next();
-				countEntries++;
+ 			while (objectIterator.hasNext()) {	
+				if(objectIterator.next()!=null) 
+					countEntries++;
 			}
 			System.out.println("Total Entries: " + countEntries);
 			reader.close();
+			return countEntries;
 		}
-		
-		catch(CensusAnalyserException e) {
+
+		catch (CensusAnalyserException e) {
 			throw new CensusAnalyserException(CensusExceptionType.INCORRECT_TYPE, e.getMessage());
-		}
-		catch(NoSuchFileException e) {
+		} catch (NoSuchFileException e) {
 			throw new CensusAnalyserException(CensusExceptionType.FILE_NOT_FOUND_TYPE, "NO FILE FOUND!!");
+		} catch (RuntimeException r) {
+			throw new CensusAnalyserException(CensusExceptionType.DELIMITER_TYPE,
+					"Other Runtime base Exception reagarding Delimiter Issue: " + r.getMessage());
+		} catch (Exception E) {
+			throw new CensusAnalyserException(CensusExceptionType.OTHER_TYPE, E.getMessage());
 		}
-		catch(RuntimeException r) {
-			throw new CensusAnalyserException(CensusExceptionType.OTHER_TYPE, "Other Runtime base Exception: "+r.getMessage());
-		}
-		catch (Exception E) {
-			E.printStackTrace();
-		}
-		return countEntries;
 	}
 
 	private static void validateInputObjectType(Object myClass) throws CensusAnalyserException {
-		if(!myClass.equals(CSVStateCensus.class)) {
-			throw new CensusAnalyserException(CensusExceptionType.INCORRECT_TYPE,"Incorrect Type");
+		if (!myClass.equals(CSVStateCensus.class)) {
+			throw new CensusAnalyserException(CensusExceptionType.INCORRECT_TYPE, "Incorrect Type");
 		}
 	}
 
