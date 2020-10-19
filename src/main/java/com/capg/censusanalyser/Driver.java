a1212459f44bb9a6ffeb02802b05b769a7b124aa
pachgadehardik <pachgadehardik@gmail.com>
2020-10-19 15:15:13 +0530

    Refactored and Added Log4j in the analyser

--- src/main/java/com/capg/censusanalyser/Driver.java
+++ src/main/java/com/capg/censusanalyser/Driver.java
@@ -2,12 +2,14 @@ package com.capg.censusanalyser;
 
 import java.io.IOException;
 
+import com.opencsv.exceptions.CsvException;
+
 /**
  * Hello world!
  *
  */
 public class Driver {
-	public static void main(String[] args) throws CensusAnalyserException {
+	public static void main(String[] args) throws CensusAnalyserException, CsvException {
 		final String INDIA_STATE_CENSUS_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IndiaStateCensusData - IndiaStateCensusData.csv";
 		final String INCORRECT_DATA_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IncorrectFile.txt";
 		final String INDIA_STATE_CODE_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IndiaStateCode - IndiaStateCode.csv";
--- src/main/java/com/capg/censusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/capg/censusanalyser/StateCensusAnalyser.java
@@ -6,50 +6,59 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
-import java.util.Spliterator;
 import java.util.stream.StreamSupport;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
 import com.capg.censusanalyser.CensusAnalyserException.CensusExceptionType;
-import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class StateCensusAnalyser {
-	
-	private final static String INDIA_STATE_CENSUS_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IndiaStateCensusData - IndiaStateCensusData.csv";
-	private final String INCORRECT_DATA_FILE = "H:\\Capgemini\\Capg_Training\\census-analyser\\IncorrectFile.txt";
 
+	private static final Logger logger = LogManager.getLogger(StateCensusAnalyser.class);
+
+	@SuppressWarnings("unchecked")
 	public static <T> int readCSVDataFile(String dataFile, Class<T> myClass)
 			throws IOException, CensusAnalyserException {
-		int countEntries =0;
-
+		Iterable<T> csvToBean = null;
+		Reader reader = null;
+		int countEntries = 0;
+		DOMConfigurator.configure("log4j.xml");
 		try {
-			Reader reader = Files.newBufferedReader(Paths.get(dataFile));
+			reader = Files.newBufferedReader(Paths.get(dataFile));
 			validateInputObjectType(myClass);
-
 			CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
 			csvToBeanBuilder.withType((Class) myClass);
 			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-			CsvToBean<T> csvToBean = csvToBeanBuilder.build();
-			Iterator<T> objectIterator = csvToBean.iterator();
-			Iterable<T> objIterable = () -> objectIterator;
-			countEntries = (int) StreamSupport.stream(objIterable.spliterator(),false).count();
-			System.out.println("Total Entries: " + countEntries);
-			reader.close();
-			return countEntries;
+			csvToBean = csvToBeanBuilder.build();
 		}
 
 		catch (CensusAnalyserException e) {
+			logger.error("Exception occured regarding Incorrect Type of POJO");
 			throw new CensusAnalyserException(CensusExceptionType.INCORRECT_TYPE, e.getMessage());
 		} catch (NoSuchFileException e) {
+			logger.error("No File Found!!");
 			throw new CensusAnalyserException(CensusExceptionType.FILE_NOT_FOUND_TYPE, "NO FILE FOUND!!");
 		} catch (RuntimeException r) {
+			logger.error("Delimiter or header issue !!");
 			throw new CensusAnalyserException(CensusExceptionType.DELIMITER_OR_HEADER_TYPE,
 					"Runtime base Exception reagarding Delimiter or any Header Issue: " + r.getMessage());
 		} catch (Exception E) {
+			logger.error("Other Exception");
 			throw new CensusAnalyserException(CensusExceptionType.OTHER_TYPE, E.getMessage());
 		}
-	}
+		
+		Iterator<T> objectIterator = csvToBean.iterator();
+		Iterable<T> objIterable = () -> objectIterator;
+		countEntries = (int) StreamSupport.stream(objIterable.spliterator(), false).count();
+		logger.info("Total Entries: " + countEntries);
+		reader.close();
+		return countEntries;
 
+	}
+	
 	private static void validateInputObjectType(Object myClass) throws CensusAnalyserException {
 		if (!(myClass.equals(CSVStateCensus.class) || myClass.equals(CSVStateCode.class))) {
 			throw new CensusAnalyserException(CensusExceptionType.INCORRECT_TYPE, "Incorrect Type");
